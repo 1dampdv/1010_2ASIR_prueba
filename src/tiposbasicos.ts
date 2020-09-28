414105ec415b37e26f316b891a6051a6c7fe5eb5
Your Name <you@example.com>
2020-09-28 12:45:55 +0200

    tipos básicos unión, arrays

---
+++ src/tiposbasicos.ts
@@ -0,0 +1,22 @@
+/*
+ver:
+https://www.typescriptlang.org/docs/handbook/basic-types.html
+*/
+let activo: boolean
+let valor: number
+let cantidad = [2, 4, 8, "h"]
+let nombre: string
+let lista: number[]
+cantidad = [2, 4, 8, "h"]
+lista = [2, 4, 8, "h"]
+let edad = 30
+activo = false
+if (!activo) {
+    console.log('NO está activo')
+}else{
+    console.log('Está activo')
+}
+
+
+
+
